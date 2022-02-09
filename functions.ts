f0dc2320f8560a62cf658a1406f08eae2782db5f
Sean Blonien <sean.blonien@parivedasolutions.com>
2022-02-09 00:29:31 -0600

    Nesting

--- functions.ts
+++ functions.ts
@@ -1,12 +1,24 @@
 import {repeat} from './utils';
 
+type Address = {
+  street: string;
+  city: string;
+  state: string;
+  zip: string;
+}
+
 type Person = {
   firstName: string;
   lastName: string;
   age: number;
+  address?: Address;
 }
 
 export const greet = (name: Person, useExclamation: boolean = false, iterations: number = 1): void => {
+  if (name.address === undefined) {
+    console.error('Address is undefined');
+    return;
+  }
   repeat(() => {
     console.log(`Hello ${name.firstName} ${name.lastName}${useExclamation ? '!' : ''}`)
   }, iterations);
